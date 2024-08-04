a8b9b71b158f611422e99806861a0aed35afc638
jcedeno <jcedeno.us+infinity@gmail.com>
2024-08-04 19:29:48 -0400

    improve responsiveness

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,20 @@ import '../styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../lib/i18n'; // Import your i18n configuration
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <I18nextProvider i18n={i18n}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </I18nextProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+      </Head>
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </I18nextProvider>
+    </>
   );
 }
 
